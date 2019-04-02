d5dcb8e2dd2aaa0ad4a26c607190629cceba9b25
Dehinde Shogbanmu <dehindeshogbanmu@yahoo.ie>
2019-04-02 16:32:39 +0000

    chart plotted

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,6 +9,9 @@ function createGraphs(error, busesData) {
         d.number_of_buses = parseInt(d.number_of_buses);
     });
     
+    
+    show_yearly_total_number_of_buses(ndx);
+    show_years_of_buses(ndx);
     show_total_number_of_buses(ndx);
     show_bus_types(ndx);
     show_bus_fuel(ndx);
@@ -44,13 +47,52 @@ function show_bus_fuel(ndx) {
         
 }
 
+function show_yearly_total_number_of_buses(ndx) {
+    
+    var date_dim = ndx.dimension(dc.pluck('number_of_buses'));
+    var total_number_of_buses_yearly = date_dim.group().reduceSum(dc.pluck('year'));
+    
+  
+    dc.lineChart('#yearly-number-of-buses')
+        .width(1700)
+        .height(300)
+        .margins({top:10, right:50, bottom:30, left:20})
+        .dimension(date_dim)
+        .group(total_number_of_buses_yearly)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Number of Buses")
+        .yAxis().ticks(4);
+}
+
+
+
 function show_total_number_of_buses(ndx) {
     
-    var buses_dim = ndx.dimension(dc.pluck('bus_type'));
+    var buses_dim = ndx.dimension(dc.pluck('year'));
     var total_buses = buses_dim.group().reduceSum(dc.pluck('number_of_buses'));
     
     dc.pieChart('#total-number-of-buses')
-        .height(500)
+        .height(450)
+        .radius(400)
+        .transitionDuration(1500)
+        .dimension(buses_dim)
+        .group(total_buses);
+    
+    dc.renderAll();
+        
+        
+}
+
+
+function show_years_of_buses(ndx) {
+    
+    var buses_dim = ndx.dimension(dc.pluck('bus_type'));
+    var total_buses = buses_dim.group().reduceSum(dc.pluck('year'));
+    
+    dc.pieChart('#years-of-buses')
+        .height(450)
         .radius(400)
         .transitionDuration(1500)
         .dimension(buses_dim)
